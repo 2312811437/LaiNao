5a5e5c3c63925e4b7bd92ee8fc43e0c6770bd940
Karl <Karl9452@126.com>
2018-03-19 09:11:34 +0800

    commit

---
+++ app/src/main/java/com/cg/lainao/MyApp.java
@@ -0,0 +1,16 @@
+package com.cg.lainao;
+
+import android.app.Application;
+
+/**
+ * MyApp
+ * Created by Administrator on 2018/3/17 0017.
+ */
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
